8c5c40fe6c6bd148e12a66f2111e3435a4d0b510
NguyenQuocThinh18 <nguyenquocthinh210404@gmail.com>
2025-10-26 21:27:21 +0700

    finalcommit

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -109,26 +109,7 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-async getProductById(req, res, next) {
-  try {
-    const token = req.headers.authorization;
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-
-    const { id } = req.params;
-    const product = await Product.findById(id);
-
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-
-    return res.status(200).json(product);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Server error" });
-  }
-}
+//
 }
 
 module.exports = ProductController;
